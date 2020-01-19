12e923fe097ad7053cb58660595fd88927bce248
Sean Asiala <asialasr@umich.edu>
2020-01-19 04:06:15 -0500

    improve code to process csv diff, exposing preexisting issues

--- CsvDiffToSheet.py
+++ CsvDiffToSheet.py
@@ -84,7 +84,7 @@ def check_change_add_and_sub(line, out_file):
 
 # TODO(sasiala): new line is broken
 def check_new_line(line, out_file):
-    is_new_line = re.match('\+ .*$', line)
+    is_new_line = re.match('^\+ .+$', line)
 
     if is_new_line:
         split_lines = line.split('\n')
@@ -97,7 +97,7 @@ def check_new_line(line, out_file):
     return False
 
 def check_deleted_line(line, out_file):
-    is_deleted_line = re.match('^- .*$', line)
+    is_deleted_line = re.match('^- .+$', line)
 
     if is_deleted_line:
         split_lines = line.split('\n')
@@ -109,64 +109,60 @@ def check_deleted_line(line, out_file):
         return True
     return False
 
-def check_compound(line, out_file):
-    line = line.split('\n')
+def check_compound(line_in, out_file):
+    line = line_in.split('\n')
     left_over = line
-    if (len(line) >= 4):
-        # TODO(sasiala): am I sure these can't be in the middle of the string?
-        if (check_change_add_and_sub('\n'.join(line[0:4]), out_file)):
-            left_over = line[4:]
-            logger.log('diff_to_sheet.log', 'Compound:Change/Add/Sub', logger.LogLevel.DEBUG)
-        elif (check_change_add('\n'.join(line[0:3]), out_file)):
-            left_over = line[3:]
-            logger.log('diff_to_sheet.log', 'Compound:Change/Add', logger.LogLevel.DEBUG)
-        elif (check_change_sub('\n'.join(line[0:3]), out_file)):
-            left_over = line[3:]
-            logger.log('diff_to_sheet.log', 'Compound:Change/Sub', logger.LogLevel.DEBUG)
+    logger.log('diff_to_sheet.log', f'Line: {line_in}', logger.LogLevel.DEBUG)
+    # TODO(sasiala): am I sure these can't be in the middle of the string?
+    # TODO(sasiala): there is a case for - .*\n+ .*$ that needs to be handled.  
+    #                It is similar to check_change_sub and can be seen in sheet 0 of test.
+    if (len(line) >= 4 and check_change_add_and_sub('\n'.join(line[0:4]), out_file)):
+        left_over = line[4:]
+        logger.log('diff_to_sheet.log', 'Compound:Change/Add/Sub', logger.LogLevel.DEBUG)
+    elif (len(line) >= 3 and check_change_add('\n'.join(line[0:3]), out_file)):
+        left_over = line[3:]
+        logger.log('diff_to_sheet.log', 'Compound:Change/Add', logger.LogLevel.DEBUG)
+    elif (len(line) >= 3 and check_change_sub('\n'.join(line[0:3]), out_file)):
+        left_over = line[3:]
+        logger.log('diff_to_sheet.log', 'Compound:Change/Sub', logger.LogLevel.DEBUG)
         
-        for i in left_over:
-            if (check_new_line(i, out_file)):
-                logger.log('diff_to_sheet.log', 'Compound:New Line', logger.LogLevel.DEBUG)
-            elif (check_deleted_line(i, out_file)):
-                logger.log('diff_to_sheet.log', 'Compound:Deleted Line', logger.LogLevel.DEBUG)
-            elif re.match('- $', i) or re.match('^\+ $', i):
-                logger.log('diff_to_sheet.log', 'Compound:Skipped empty +/- line', logger.LogLevel.DEBUG)
-            else:
-                # unexpected format in diff
-                logger.log('diff_to_sheet.log', 'Compound:Curious (unexpected diff format)...', logger.LogLevel.ERROR)
-                logger.log('diff_to_sheet.log', i, logger.LogLevel.ERROR)
-                logger.log('diff_to_sheet.log', '/Curious', logger.LogLevel.ERROR)
-                # TODO(sasiala): return False
-        return True
-    return False
+    for i in left_over:
+        # TODO(sasiala): should the check for an empty line be here? 
+        #                Or should that be fixed when generating the file used as input here?
+        if re.match('^- $', i) or re.match('^\+ $', i) or re.match('^  $', i) or re.match('^$', i):
+            logger.log('diff_to_sheet.log', 'Compound:Skipped empty +/- line', logger.LogLevel.DEBUG)
+        elif (check_new_line(i, out_file)):
+            logger.log('diff_to_sheet.log', 'Compound:New Line', logger.LogLevel.DEBUG)
+        elif (check_deleted_line(i, out_file)):
+            logger.log('diff_to_sheet.log', 'Compound:Deleted Line', logger.LogLevel.DEBUG)
+        elif re.match('^  .+$', i):
+            logger.log('diff_to_sheet.log', 'Compound:No Change', logger.LogLevel.DEBUG)
+            out_file.write('No Change,')
+            out_file.write(re.sub('^  ', '', i))
+            out_file.write('\n')
+        else:
+            # unexpected format in diff
+            logger.log('diff_to_sheet.log', 'Compound:Curious (unexpected diff format)...', logger.LogLevel.ERROR)
+            logger.log('diff_to_sheet.log', i, logger.LogLevel.ERROR)
+            logger.log('diff_to_sheet.log', '/Curious', logger.LogLevel.ERROR)
+            # TODO(sasiala): return False
+    return True
 
 def diff_to_sheet(csv_diff_path, out_path):
+    logger.log('diff_to_sheet.log', f'Creating sheet csv for {csv_diff_path}', logger.LogLevel.DEBUG)
     with open(out_path, 'w') as out_file:
         with open(csv_diff_path, 'r') as csv_diff:
             lines = csv_diff.read().split('\n  \n')
             for line in lines:
-                change_sub_split = re.split('\n\?.*\n\+ ', line) # TODO
-
-                if check_change_sub(line, out_file):
-                    logger.log('diff_to_sheet.log', 'Change/Sub', logger.LogLevel.DEBUG)
-                elif check_change_add(line, out_file):
-                    logger.log('diff_to_sheet.log', 'Change/Add', logger.LogLevel.DEBUG)
-                elif check_change_add_and_sub(line, out_file):
-                    logger.log('diff_to_sheet.log', 'Change/Add/Sub', logger.LogLevel.DEBUG)
-                elif check_compound(line, out_file):
-                    logger.log('diff_to_sheet.log', 'Compound', logger.LogLevel.DEBUG)
-                elif len(change_sub_split) == 1:
-                    if len(line.split('  ')) == 2:
-                        logger.log('diff_to_sheet.log', 'No Change', logger.LogLevel.DEBUG)
-                        out_file.write('No Change,')
-                        out_file.write(line.split('  ')[1])
-                        out_file.write('\n')
-                else:
+                if not check_compound(line, out_file):
                     # unexpected format in diff
                     logger.log('diff_to_sheet.log', 'Curious (unexpected diff format)...', logger.LogLevel.ERROR)
                     logger.log('diff_to_sheet.log', line, logger.LogLevel.ERROR)
                     logger.log('diff_to_sheet.log', '/Curious', logger.LogLevel.ERROR)
                     # TODO(sasiala): return False
+            logger.log('diff_to_sheet.log', f'Successfully completed sheet csv for {csv_diff_path}', logger.LogLevel.DEBUG)
             return True
+        logger.log('diff_to_sheet.log', f'Failed to create sheet csv for {csv_diff_path}.  Error: could not open csv_diff', logger.LogLevel.ERROR)
         return False
+    logger.log('diff_to_sheet.log', f'Failed to create sheet csv for {csv_diff_path}.  Error: could not open out_path', logger.LogLevel.ERROR)
     return False
