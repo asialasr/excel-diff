b08ac70e4b4d2066f933bcb31b72f0b57cadb41c
Sean Asiala <asialasr@umich.edu>
2020-02-27 03:12:09 -0500

    Add output for GUI, improve maintainability

--- excel_diff.py
+++ excel_diff.py
@@ -21,6 +21,7 @@ RHS_TEMP_FOLDER=TEMP_FOLDER+'/rhs'
 SHEET_DIFF_TEMP_FOLDER=TEMP_FOLDER+'/diff_sheets'
 CSV_DIFF_TEMP_FOLDER=TEMP_FOLDER+'/csv_diff'
 OUTPUT_FOLDER='output'
+GUI_OUTPUT_FOLDER=f'{OUTPUT_FOLDER}/gui_out'
 
 save_temp=False
 
@@ -60,9 +61,17 @@ def setup_temp_directories():
     os.mkdir(SHEET_DIFF_TEMP_FOLDER)
     os.mkdir(CSV_DIFF_TEMP_FOLDER)
 
-def setup_output_directory():
+def remove_output_directories():
+    if os.path.exists(OUTPUT_FOLDER):
+        shutil.rmtree(OUTPUT_FOLDER)
+
+def setup_output_directory(gui_output):
+    remove_output_directories()
+
     if not os.path.exists(OUTPUT_FOLDER):
         os.mkdir(OUTPUT_FOLDER)
+    if gui_output and not os.path.exists(GUI_OUTPUT_FOLDER):
+        os.mkdir(GUI_OUTPUT_FOLDER)
 
 def generate_csvs_for_xlsx(xlsx_path, temp_path):
     with xlrd.open_workbook(xlsx_path) as xlsx_file:
@@ -77,6 +86,17 @@ def generate_csvs_for_xlsx(xlsx_path, temp_path):
             return True
         return False
     return False
+
+def split_intermediate_renamed_sheet(intermediate_sheet_name):
+    return intermediate_sheet_name.split(']')
+
+def format_intermediate_renamed_sheet(lhs_name, rhs_name):
+    return f'{lhs_name}]{rhs_name}'
+
+EXISTING_SHEET='b'
+RENAMED_SHEET='r'
+NEW_SHEET='n'
+DELETED_SHEET='d'
 
 def get_unified_sheets(lhs_sheet_file, lhs_temp_path, rhs_sheet_file, rhs_temp_path):
     unified = []
@@ -93,9 +113,9 @@ def get_unified_sheets(lhs_sheet_file, lhs_temp_path, rhs_sheet_file, rhs_temp_p
             continue
 
         if re.match('^  .*$', diff[i]):
-            unified.append(['b', re.sub('^  ', '', diff[i])])
+            unified.append([EXISTING_SHEET, re.sub('^  ', '', diff[i])])
         elif re.match('^\+ .*$', diff[i]):
-            unified.append(['n', re.sub('^\+ ', '', diff[i])])
+            unified.append([NEW_SHEET, re.sub('^\+ ', '', diff[i])])
         elif re.match('^- .*$', diff[i]):
             this_line_sub = re.sub('^- ', '', diff[i])
             if i < len(diff) - 1 and re.match('^\+ .*$', diff[i+1]):
@@ -107,31 +127,37 @@ def get_unified_sheets(lhs_sheet_file, lhs_temp_path, rhs_sheet_file, rhs_temp_p
                         seq = difflib.SequenceMatcher(None, lhs_csv.read(), rhs_csv.read())
                 
                 if seq.quick_ratio() > .5:
-                    unified.append(['r', f'{this_line_sub}]{next_line_sub}'])
+                    unified.append([RENAMED_SHEET, format_intermediate_renamed_sheet(this_line_sub, next_line_sub)])
                     skip_next = True
                 else:
-                    unified.append(['d', this_line_sub])
+                    unified.append([DELETED_SHEET, this_line_sub])
             else:
-                unified.append(['d', this_line_sub])
+                unified.append([DELETED_SHEET, this_line_sub])
         else:
             print('Unexpected format in unified sheets')
 
     return unified
 
+def format_unchanged_sheetname(sheet_name):
+    return f'_UNCH_{sheet_name}'
+
+def format_renamed_sheetname(lhs_name, rhs_name):
+    # prints length of original sheet name to indicate where split occurs
+    return f'_RNM{len(lhs_name)}_{lhs_name}_{rhs_name}'
+
 def check_modified_sheet(workbook, sheet_pair, output_sheet_path, sheet_name):
-    if sheet_pair[0]=='b' or sheet_pair[0]=='r':
+    if sheet_pair[0]==EXISTING_SHEET or sheet_pair[0]==RENAMED_SHEET:
         left_csv_path=''
         right_csv_path=''
-        if sheet_pair[0]=='b':
+        if sheet_pair[0]==EXISTING_SHEET:
             left_csv_path=f'{LHS_TEMP_FOLDER}/{sheet_pair[1]}.csv'
             right_csv_path=f'{RHS_TEMP_FOLDER}/{sheet_pair[1]}.csv'
-            sheet_name.append(sheet_pair[1])
+            sheet_name.append(format_unchanged_sheetname(sheet_pair[1]))
         else:
-            temp_sheet_names=sheet_pair[1].split(']')
+            temp_sheet_names=split_intermediate_renamed_sheet(sheet_pair[1])
             left_csv_path=f'{LHS_TEMP_FOLDER}/{temp_sheet_names[0]}.csv'
             right_csv_path=f'{RHS_TEMP_FOLDER}/{temp_sheet_names[1]}.csv'
-            # prints length of original sheet name to indicate where split occurs
-            sheet_name.append(f'_RNM{len(temp_sheet_names[0])}_{temp_sheet_names[0]}_{temp_sheet_names[1]}')
+            sheet_name.append(format_renamed_sheetname(temp_sheet_names[0], temp_sheet_names[1]))
 
         output_sheet_path.append(f'{SHEET_DIFF_TEMP_FOLDER}/{sheet_name[0]}.csv')
 
@@ -144,9 +170,12 @@ def check_modified_sheet(workbook, sheet_pair, output_sheet_path, sheet_name):
         return True
     return False
 
+def format_new_sheetname(sheet_name):
+    return f'_NEW_{sheet_name}'
+
 def check_new_sheet(workbook, sheet_pair, output_sheet_path, sheet_name):
-    if sheet_pair[0]=='n':
-        sheet_name.append(f'_NEW_{sheet_pair[1]}')
+    if sheet_pair[0]==NEW_SHEET:
+        sheet_name.append(format_new_sheetname(sheet_pair[1]))
         output_sheet_path.append(f'{RHS_TEMP_FOLDER}/{sheet_pair[1]}_proc.csv')
 
         # TODO(sasiala): a) will this work correctly? b) may need to fix original csv gen & thus fix everything else
@@ -156,9 +185,12 @@ def check_new_sheet(workbook, sheet_pair, output_sheet_path, sheet_name):
                 return True
     return False
 
+def format_deleted_sheetname(sheet_name):
+    return f'_DEL_{sheet_name}'
+
 def check_deleted_sheet(workbook, sheet_pair, output_sheet_path, sheet_name):
-    if sheet_pair[0]=='d':
-        sheet_name.append(f'_DEL_{sheet_pair[1]}')
+    if sheet_pair[0]==DELETED_SHEET:
+        sheet_name.append(format_deleted_sheetname(sheet_pair[1]))
         output_sheet_path.append(f'{LHS_TEMP_FOLDER}/{sheet_pair[1]}_proc.csv')
 
         # TODO(sasiala): a) will this work correctly? b) may need to fix original csv gen & thus fix everything else
@@ -189,10 +221,73 @@ def process_sheet(workbook, sheet_pair):
         return False
     return True
 
-def process_xlsx(lhs_path, rhs_path):
+def generate_gui_sheet_names(unified_sheets):
+    with open(f'{GUI_OUTPUT_FOLDER}/sheets.txt', 'w') as fout:
+        for i in unified_sheets:
+            if i[0]==EXISTING_SHEET:
+                fout.write(f'{format_unchanged_sheetname(i[1])}.csv\n')
+            elif i[0]==RENAMED_SHEET:
+                sheet_names = split_intermediate_renamed_sheet(i[1])
+                fout.write(f'{format_renamed_sheetname(sheet_names[0], sheet_names[1])}.csv\n')
+            elif i[0]==NEW_SHEET:
+                fout.write(f'{format_new_sheetname(i[1])}.csv\n')
+            elif i[0]==DELETED_SHEET:
+                fout.write(f'{format_deleted_sheetname(i[1])}.csv\n')
+            else:
+                logger.log('excel_diff.log', 'Error: unrecognized sheet pair type', logger.LogLevel.ERROR)
+                return False
+        return True
+    logger.log('excel_diff.log', 'Error: unable to open sheet_names.txt for gui out', logger.LogLevel.ERROR)
+    return False
+
+def get_gui_destination_filename(sheet_pair):
+    if sheet_pair[0]==EXISTING_SHEET:
+        return f'{format_unchanged_sheetname(sheet_pair[1])}.csv'
+    elif sheet_pair[0]==RENAMED_SHEET:
+        sheet_names = split_intermediate_renamed_sheet(sheet_pair[1])
+        return f'{format_renamed_sheetname(sheet_names[0], sheet_names[1])}.csv'
+    elif sheet_pair[0]==NEW_SHEET:
+        return f'{format_new_sheetname(sheet_pair[1])}.csv'
+    elif sheet_pair[0]==DELETED_SHEET:
+        return f'{format_deleted_sheetname(sheet_pair[1])}.csv'
+    logger.log('excel_diff.log', 'Error: unrecognized sheet pair type', logger.LogLevel.ERROR)
+    return f''
+
+def find_sheet(sheet_pair):
+    ret = f''
+    if sheet_pair[0]==EXISTING_SHEET:
+        ret = f'{SHEET_DIFF_TEMP_FOLDER}/'
+    elif sheet_pair[0]==RENAMED_SHEET:
+        ret = f'{SHEET_DIFF_TEMP_FOLDER}/'
+    elif sheet_pair[0]==NEW_SHEET:
+        return f'{RHS_TEMP_FOLDER}/{sheet_pair[1]}.csv'
+    elif sheet_pair[0]==DELETED_SHEET:
+        return f'{LHS_TEMP_FOLDER}/{sheet_pair[1]}.csv'
+    else:
+        logger.log('excel_diff.log', 'Error: unrecognized sheet pair type', logger.LogLevel.ERROR)
+        return f''
+    
+    return ret + get_gui_destination_filename(sheet_pair)
+
+def copy_sheets(unified_sheets, source_dir, destination_dir):
+    for sheet in unified_sheets:
+        sheet_path = find_sheet(sheet)
+        if not sheet_path == '':
+            shutil.copy(sheet_path, f'{destination_dir}/{get_gui_destination_filename(sheet)}')
+        else:
+            logger.log('excel_diff.log', 'Error: file not found in copy_sheets', logger.LogLevel.ERROR)
+            return False
+    return True
+
+def generate_gui_output(unified_sheets):
+    generate_gui_sheet_names(unified_sheets)
+    copy_sheets(unified_sheets, SHEET_DIFF_TEMP_FOLDER, GUI_OUTPUT_FOLDER)
+
+
+def process_xlsx(lhs_path, rhs_path, gui_output):
     setup_temp_directories()
     logger.initialize_directory_structure()
-    setup_output_directory()
+    setup_output_directory(gui_output)
 
     generate_csvs_for_xlsx(lhs_path, LHS_TEMP_FOLDER)
     generate_csvs_for_xlsx(rhs_path, RHS_TEMP_FOLDER)
@@ -213,6 +308,9 @@ def process_xlsx(lhs_path, rhs_path):
         process_sheet(workbook, sheet_pair)
     workbook.close()
 
+    if gui_output:
+        generate_gui_output(unified_sheets)
+
     # TODO(sasiala): add logging
 
     if not save_temp:
@@ -227,6 +325,7 @@ def main():
     # TODO(sasiala): rethink argument naming
     parser.add_argument('-v', '--verbose', action='count', default=0, help='Increase Verbosity')
     parser.add_argument('-st', '--save-temp', action='store_true', help='Save all temporary files')
+    parser.add_argument('-g', '--gui-out', action='store_true', help='Output files for GUI instead of xlsx')
     args = parser.parse_args()
 
     try:
@@ -238,7 +337,7 @@ def main():
     global save_temp
     save_temp=args.save_temp
 
-    process_xlsx(args.lhspath, args.rhspath)
+    process_xlsx(args.lhspath, args.rhspath, args.gui_out)
 
 if __name__ == "__main__":
     # execute only if run as a script
